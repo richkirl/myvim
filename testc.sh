ca3af63ad299d8a8485814c3a33b4fa4764647ab
richkirl <61930048+richkirl@users.noreply.github.com>
2022-07-23 05:45:22 +0300

    Add files via upload

---
+++ testc.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/sh
+echo "#include<stdio.h> \nint main(){ \nprintf(); \nreturn 0; \n} \n"
+
---
+++ testcpp.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/sh
+echo "#include<iostream> \nint main(){ \nstd::cout<< <<std::endl; \nreturn 0; \n} \n"
